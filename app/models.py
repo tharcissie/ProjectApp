9436d344bf1dd48a46fb30029c0f7533bf2898cf
tharcissie <tharcissieidufashe@gmail.com>
2021-01-11 17:09:35 +0200

    models

--- app/models.py
+++ app/models.py
@@ -1,3 +1,88 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from pyuploadcare.dj.models import ImageField
+import datetime as dt
 
-# Create your models here.
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='user')
+    profile_picture = models.ImageField(upload_to='images/', default='default.png')
+    bio = models.TextField(max_length=500, default="My Bio", blank=True)
+    contact = models.EmailField(max_length=100, blank=True)
+
+    def __str__(self):
+        return self.user
+
+    @receiver(post_save, sender=User)
+    def create_user_profile(sender, instance, created, **kwargs):
+        if created:
+            Profile.objects.create(user=instance)
+
+    @receiver(post_save, sender=User)
+    def save_user_profile(sender, instance, **kwargs):
+        instance.profile.save()
+
+
+class Project(models.Model):
+    project_name = models.CharField(max_length=155)
+    link = models.URLField(max_length=255)
+    details = models.TextField(max_length=255)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="user")
+    date = models.DateTimeField(auto_now_add=True, blank=True)
+
+    def __str__(self):
+        return self.project_name
+
+    def delete_post(self):
+        self.delete()
+
+    @classmethod
+    def search_project(cls, project_name):
+        return cls.objects.filter(title__icontains=project_name).all()
+
+    @classmethod
+    def all_posts(cls):
+        return cls.objects.all()
+
+    def save_post(self):
+        self.save()
+
+
+
+
+class Rating(models.Model):
+    rating = (
+        (1, '1'),
+        (2, '2'),
+        (3, '3'),
+        (4, '4'),
+        (5, '5'),
+        (6, '6'),
+        (7, '7'),
+        (8, '8'),
+        (9, '9'),
+        (10, '10'),
+    )
+    project = models.ForeignKey(Project, on_delete=models.CASCADE, related_name='project', null=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, related_name='user')
+    design = models.IntegerField(choices=rating, default=0, blank=True)
+    usability = models.IntegerField(choices=rating, blank=True)
+    content = models.IntegerField(choices=rating, blank=True)
+    score = models.FloatField(default=0, blank=True)
+    design_average = models.FloatField(default=0, blank=True)
+    usability_average = models.FloatField(default=0, blank=True)
+    content_average = models.FloatField(default=0, blank=True)
+    
+
+    def save_rating(self):
+        self.save()
+
+    @classmethod
+    def get_ratings(cls, id):
+        rating = Rating.objects.filter(id=id).all()
+        return rating
+
+    def __str__(self):
+        return self.project
